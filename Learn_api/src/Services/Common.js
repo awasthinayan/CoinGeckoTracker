00fcd920074304067432f40bb6d796532cfe21df
awasthinayan <nayanawasthi2015@gmail.com>
2026-01-17 22:30:13 +0530

    updated api end points

--- Learn_api/src/Services/Common.js
+++ Learn_api/src/Services/Common.js
@@ -1,3 +1,3 @@
 export const COINGECKO_API_URL="https://api.coingecko.com/api/v3";
 
-export const COINGECKO_Auth_API="https://crypto-ttracker-auth.onrender.com/V1/api";
+export const COINGECKO_Auth_API="https://crypto-ttracker-auth.onrender.com/v1/api";
